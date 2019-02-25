8e86e2e7dedcf4fff2e3a8671035f68ff3d266bc
Rick de Hoop <iceicerickert@users.noreply.github.com>
2019-02-25 13:23:29 +0100

    Update restore_original_db.sh

--- scripts/restore_original_db.sh
+++ scripts/restore_original_db.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "Copy pluggable database..."
-export ORACLE_HOME="/u01/app/oracle/product/11.2.0/xe"
-/u01/app/oracle/product/11.2.0/xe/bin/sqlplus c##dba/dba@cdach01 <<SQL
+export ORACLE_HOME="/var/jenkins_home/tools/oracle"
+cd /var/jenkins_home/tools/oracle
+ls -la
+/var/jenkins_home/tools/oracle/instantclient_12_2/sqlplus c##dba/dba@cdqc01
   set serveroutput on size unlimited;
-  exec sys.pdb_maintenance.show_pdbs;
-  exec sys.pdb_maintenance.clone_pdb ('PD_ACH01_ACH_0705_01','PD_ACH01_ACH_0705_03','Y')
 SQL
