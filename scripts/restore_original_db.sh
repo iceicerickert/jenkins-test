be739890dd7d795ee0d78c13a2b6a9951c4c3907
Rick de Hoop <iceicerickert@users.noreply.github.com>
2019-02-25 14:06:40 +0100

    Update restore_original_db.sh

--- scripts/restore_original_db.sh
+++ scripts/restore_original_db.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo "Copy pluggable database..."
 export ORACLE_HOME="/u01/app/oracle/product/11.2.0/xe"
+cd /u01/app/oracle
+ls -la
   /u01/app/oracle/product/11.2.0/xe/bin/sqlplus c##dba/dba@cdqc01 <<SQL
   set serveroutput on size unlimited;
 SQL
